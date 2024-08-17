d94cb622c204819554dde3becbd0a444b0fed5e7
MD. Shariar Hossain Sun <75157104+sun01822@users.noreply.github.com>
2024-08-17 20:19:57 +0600

    185._Department_Top_Three_Salaries

--- 185._Department_Top_Three_Salaries.sql
+++ 185._Department_Top_Three_Salaries.sql
@@ -14,7 +14,7 @@ select t.name as Department, e.name as Employee, e.salary as Salary from Temp as
 on t.id = e.departmentId
 where t.highest_3 <= e.salary;
 
-#Using conditions
+#Using DENSE_RANK and conditions
  select Department, Employee, Salary from (
  SELECT d.name AS Department
         , e.name AS Employee
