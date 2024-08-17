c7ec74813435ef5ed94af7093ad516a73465faf4
sun01822 <mdshariarhossainsun01822@gmail.com>
2024-08-17 20:14:05 +0600

    185._Department_Top_Three_Salaries

---
+++ 185._Department_Top_Three_Salaries.sql
@@ -0,0 +1,25 @@
+#Using subquery 
+with Temp as(
+    select *, (
+    select salary from (
+    select distinct(salary) from Employee 
+    where departmentId = d.id
+    order by salary desc
+    limit 3) as new 
+    order by salary asc
+    limit 1) as highest_3 from Department as d
+)
+
+select t.name as Department, e.name as Employee, e.salary as Salary from Temp as t join Employee as e
+on t.id = e.departmentId
+where t.highest_3 <= e.salary;
+
+#Using conditions
+ select Department, Employee, Salary from (
+ SELECT d.name AS Department
+        , e.name AS Employee
+        , e.salary
+        , DENSE_RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC) AS ranker
+    FROM Employee e JOIN Department d ON e.DepartmentId= d.Id) as Temp
+    where ranker <= 3;
+
